0bdeb14724f57dd11b0ee6282525251be9e5b2ef
oleg <oleg.svinarcuk@harmonity-group.com>
2023-12-15 11:52:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const modelRef = useRef();
+  const audio = new Audio("/jingle.mp3");
+  const [showArButton, setShowArbutton] = useState(false);
+  useEffect(() => {
+    if (modelRef.current) {
+      modelRef.current.addEventListener("load", () => {
+        setShowArbutton(true);
+      });
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="App-header">slon</header>
+      <model-viewer
+        id="modelViewer"
+        src="/output.glb"
+        ios-src="/slon.usdz"
+        camera-controls
+        ar
+        ar-modes="webxr"
+        ref={(ref) => {
+          modelRef.current = ref;
+        }}
+      ></model-viewer>
+      {showArButton && (
+        <button
+          style={{
+            color: "yellow",
+            backgroundColor: "blue",
+            border: "none",
+            outline: "none",
+            height: "100px",
+            width: "300px",
+            fontSize: "24px",
+          }}
+          onClick={() => {
+            modelRef.current.activateAR();
+            setTimeout(() => {
+              modelRef.current.play();
+              audio.play();
+            }, 3000);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Pusti slona u ples
+        </button>
+      )}
     </div>
   );
 }
